31c034c8909b1d0e233b669429a9cee1afa5ed46
Gem Newman <spurll@gmail.com>
2013-03-11 13:51:58 -0500

    Added author and license information.

--- tournament.py
+++ tournament.py
@@ -1,3 +1,6 @@
+# Written by Gem Newman. This work is licensed under a Creative Commons
+# Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+
 # To Implement:
 #
 #   An "undo" fuction would be fairly trivial (and useful!) to implement!
@@ -18,12 +21,12 @@
 #   If players have been seated, and THEN people drop (and are not reseated)
 #   pairings are FUCKED.
 
-
 # More information about byes, tiebreakers, etc. can be found here:
 # http://www.wizards.com/ContentResources/Wizards/WPN/Main/Documents/Magic_The_Gathering_Tournament_Rules_PDF2.pdf
 
 
 import argparse, random, math
+from table import table, menu
 
 
 DEFAULT_PLAYERS = {"Gem", "Curt", "Matt", "Cozmin", "Eric", "Brendan",
@@ -180,10 +183,10 @@ def tournament(players=None):
 
     items = MAIN_MENU.keys()
     items.sort()
-    call = menu("Main Menu", items, input_range=range(1,len(items) + 1))
+    call = menu("Main Menu", items, input_range=range(1, len(items) + 1))
     while MAIN_MENU[items[int(call) - 1]]:
         MAIN_MENU[items[int(call) - 1]](players)
-        call = menu("Main Menu", items, input_range=range(1,len(items) + 1))
+        call = menu("Main Menu", items, input_range=range(1, len(items) + 1))
 
 
 def seat_players(players):
@@ -494,81 +497,6 @@ def drop_player(players):
                     "Dropped?"], footer="Enter a name to drop that player "
                     "(ENTER for none).", input_range=players.keys()+[""])
         if drop: players[drop].drop()
-
-
-def menu(title, *items, **options):
-    """
-    Creates a menu, asking the user to select an option, then returns that
-    option.
-
-    title: string, the title of the menu.
-    *items: list, the menu items to display; multiple lists are acceptable.
-    **headers: list, column headers for each column in items.
-    **footer: string, the footer to display before the prompt at the bottom.
-    **input_range: list, a list of acceptable inputs; if user input string is
-                   not in this list, input is rejected.
-    """
-    headers = options["headers"] if "headers" in options.keys() else None
-    footer = options["footer"] if "footer" in options.keys() else None
-    input_range = [str(i) for i in options["input_range"]] if "input_range"   \
-                   in options.keys() else None
-
-    table(title, *items, headers=headers, footer=footer)
-
-    choice = raw_input(">> ")
-    while input_range and not choice in input_range:
-        choice = raw_input(">> ")
-
-    return choice
-
-
-def table(title, *columns, **options):
-    """
-    Creates a table.
-    title: string, the title of the table.
-    *columns: tuples, with each tuple representing a column (and each item in
-              each tuple representing a cell in the table).
-    **headers: list, column headers for each column in items.
-    **footer: string, the footer to display before the prompt at the bottom.
-    """
-    headers = options["headers"] if "headers" in options.keys() else None
-    footer = options["footer"] if "footer" in options.keys() else None
-
-    column_widths = [max(max([len(str(item)) for item in c]), 1) for c in
-                     columns]
-    if headers:
-        column_widths = [max(w) for w in zip(column_widths, [len(h) for h in
-                         headers])]
-
-    total_width = sum(column_widths) + (3 * (len(column_widths) - 1))
-    if total_width < len(title):
-        diff = len(title) - total_width
-        column_widths[0] += diff
-        total_width += diff
-
-    rows = zip(*columns)
-
-    print "+{}+".format("-" * (total_width + 2))
-    print "| {:^{}} |".format(title, total_width)
-
-    dividers = ["-" * (w + 2) for w in column_widths]
-    print "+{}+".format("+".join(dividers))
-
-    if headers:
-        cells = [" {:{}} |".format(*column_width) for column_width in
-                 zip(headers, column_widths)]
-        print "".join(["|"] + cells)
-        print "+{}+".format("+".join(dividers))
-
-    for row in rows:
-        cells = [" {:{}} |".format(*column_width) for column_width in zip(row,
-                 column_widths)]
-        print "".join(["|"] + cells)
-
-    print "+{}+".format("+".join(dividers))
-
-    if footer:
-        print footer
 
 
 def is_int(value):
