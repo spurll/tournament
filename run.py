10c761f3d32d7a9750aacda18e265f0e3ddfc29e
Gem Newman <spurll@gmail.com>
2014-08-21 08:55:16 -0500

    Added .gitignore. Changed background colour.

--- run.py
+++ run.py
@@ -9,17 +9,18 @@ from argparse import ArgumentParser
 from app import app, views
 
 
-description = "Runs the Flask server for the Magic tournament program."
-parser = ArgumentParser(description=description)
-parser.add_argument("-t", "--test", help="Changes host information to allow "
-                    "access via localhost.", action="store_true")
-parser.add_argument("-n", "--nodebug", help="Turns off server debug settings, "
-                    "including the reloader.", action="store_true")
-args = parser.parse_args()
+if __name__ == '__main__':
+    description = "Runs the Flask server for the Magic tournament program."
+    parser = ArgumentParser(description=description)
+    parser.add_argument("-t", "--test", help="Changes host information to allow "
+                        "access via localhost.", action="store_true")
+    parser.add_argument("-n", "--nodebug", help="Turns off server debug settings, "
+                        "including the reloader.", action="store_true")
+    args = parser.parse_args()
 
-if args.test:
-    app.config["SERVER_NAME"] = app.config["TEST_SERVER_NAME"]
-    app.config["HOST"] = app.config["TEST_HOST"]
+    if args.test:
+        app.config["SERVER_NAME"] = app.config["TEST_SERVER_NAME"]
+        app.config["HOST"] = app.config["TEST_HOST"]
 
-app.run(app.config["HOST"], app.config["PORT"], debug=not args.nodebug)
+    app.run(app.config["HOST"], app.config["PORT"], debug=not args.nodebug)
 
