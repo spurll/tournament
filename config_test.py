31a0d7abb522accab430a960ae38e28211b204b2
Gem Newman <spurll@gmail.com>
2014-09-18 14:08:13 -0500

    Refactored server configuration stuff to be a bit more user-friendly.

--- config_test.py
+++
@@ -1,6 +0,0 @@
-from config import PORT
-
-
-# Local Testing (Must be 0.0.0.0 for external availability.)
-HOST = "127.0.0.1"
-SERVER_NAME = "localhost:{}".format(PORT)
--- run.py
+++ run.py
@@ -7,14 +7,16 @@
 from argparse import ArgumentParser
 
 from tournament import app
-import config_test
 
 
 if __name__ == '__main__':
     description = "Runs the Flask server for the Magic tournament program."
     parser = ArgumentParser(description=description)
-    parser.add_argument("-t", "--test", help="Changes host information to "
-                        "allow access via localhost.", action="store_true")
+    parser.add_argument("-0", "--public", help="Makes the server world-"
+                        "accessible by hosting at 0.0.0.0.",
+                        action="store_true")
+    parser.add_argument("-p", "--port", help="Defines the port. Defaults to "
+                        "9999.", type=int, default=9999)
     parser.add_argument("-d", "--debug", help="Turns server debug mode on. "
                         "(Not recommended for world-accesible servers!)",
                         action="store_true")
@@ -24,9 +26,6 @@ if __name__ == '__main__':
                         action="store_true")
     args = parser.parse_args()
 
-    if args.test:
-        app.config.from_object(config_test)
-
-    app.run(app.config["HOST"], app.config["PORT"], use_debugger=args.debug,
-            use_reloader=args.reload)
+    app.run(host="0.0.0.0" if args.public else "localhost", port=args.port,
+            use_debugger=args.debug, use_reloader=args.reload)
 
