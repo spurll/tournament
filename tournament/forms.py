bce014f665fa6d32c3767248f910fe54af6b5a7a
Gem Newman <spurll@gmail.com>
2016-12-19 10:58:31 -0600

    Fix deprecated import syntax

--- tournament/forms.py
+++ tournament/forms.py
@@ -1,23 +1,23 @@
-from flask.ext.wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, BooleanField, HiddenField, PasswordField
 from wtforms.fields.html5 import IntegerField
 from wtforms.validators import Required, NumberRange
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = TextField("Username", validators=[Required()])
     password = PasswordField("Password", validators=[Required()])
     remember = BooleanField("Remember Me", default=False)
 
 
-class CreateForm(Form):
+class CreateForm(FlaskForm):
     name = TextField(label="Tournament Name", default="Tournament",
                      validators=[Required()])
     players = HiddenField(label="Players", validators=[Required()])
     add = TextField(label="Add Player")
 
 
-class ReportForm(Form):
+class ReportForm(FlaskForm):
     match = HiddenField(label="Match", validators=[Required()])
     seat_1 = IntegerField(default=0, validators=[NumberRange(min=0)])
     seat_2 = IntegerField(default=0, validators=[NumberRange(min=0)])
--- tournament/views.py
+++ tournament/views.py
@@ -3,7 +3,7 @@
 
 
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask_login import login_user, logout_user, current_user, login_required
 from math import floor, ceil
 from random import shuffle
 import ldap3
