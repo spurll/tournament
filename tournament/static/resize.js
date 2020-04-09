cd0bd15c95fed94beef3c8dbfe48335e575da4cb
Gem Newman <spurll@gmail.com>
2020-04-09 11:29:24 -0500

    Fix a few issues missed in the last commit

--- tournament/static/resize.js
+++ tournament/static/resize.js
@@ -1,9 +1,16 @@
 function resize() {
-  if ($(window).width() < 700) {
+  const width = $(window).width();
+
+  if (width < 700) {
     $('body').addClass('small-screen');
+    $('body').removeClass('medium-screen');
+  }
+  else if (width < 900) {
+    $('body').addClass('medium-screen');
+    $('body').removeClass('small-screen');
   }
   else {
-    $('body').removeClass('small-screen');
+    $('body').removeClass('small-screen medium-screen');
   }
 
   // Resizes the title buffer which pushes content down below the floating title bar
